546203cff98dae33fd553dc4e89ad44548da18ca
Alejandro Morales <vamg008@gmail.com>
2013-10-09 23:14:16 -0600

    k -> key

--- print_array.rs
+++ print_array.rs
@@ -1,13 +1,14 @@
 fn main() {
   let vec = [1,2,3];
-  prt_vctr(vec);
   let str_vec = [~"ho", ~"la"];
+  prt_vctr(vec);
   prt_vctr(str_vec);
 }
 
 
 fn prt_vctr<T: ToStr>(arr: &[T]){
-  for k in arr.iter(){
-    println((*k).to_str())
+  for key in arr.iter(){
+    println((*key).to_str())
   }
 }
+
